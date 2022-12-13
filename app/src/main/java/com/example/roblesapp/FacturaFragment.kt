576f0e70152760d9b2778249fdb89d4119935e0a
Melina <melina.llacua@gmail.com>
2022-12-13 11:17:38 -0500

    modal show de soles

--- app/src/main/java/com/example/roblesapp/FacturaFragment.kt
+++ app/src/main/java/com/example/roblesapp/FacturaFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_factura.*
 
 class FacturaFragment : Fragment() {
 
@@ -15,6 +18,17 @@ class FacturaFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_factura, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnsoles.setOnClickListener{
+            val showsoles = SolesFragment()
+            showsoles.show((activity as AppCompatActivity).supportFragmentManager,"shop")
+
+        }
+
     }
 
 }
