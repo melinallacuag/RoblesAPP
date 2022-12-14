348a341ccd8ac480f9dc2467046e21bcb74350f0
Melina <melina.llacua@gmail.com>
2022-12-14 17:34:24 -0500

    card de login

--- app/src/main/java/com/example/roblesapp/Login.kt
+++ app/src/main/java/com/example/roblesapp/Login.kt
@@ -3,11 +3,14 @@ package com.example.roblesapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        btningresar.setOnClickListener{
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
