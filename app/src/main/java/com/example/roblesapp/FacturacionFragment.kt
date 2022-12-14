4b97c74fd5431e7cee10869152cb9a12f5e8bbff
Melina <melina.llacua@gmail.com>
2022-12-14 12:22:21 -0500

    se activo los datos del modal factura cada boton

--- app/src/main/java/com/example/roblesapp/FacturacionFragment.kt
+++ app/src/main/java/com/example/roblesapp/FacturacionFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_boleta.view.*
+import kotlinx.android.synthetic.main.fragment_facturacion.view.*
 import kotlinx.android.synthetic.main.fragment_nota_despacho.*
+import kotlinx.android.synthetic.main.fragment_soles.view.*
+import kotlinx.android.synthetic.main.fragment_soles.view.btncerrar
 
 
 class FacturacionFragment : DialogFragment() {
@@ -17,7 +22,22 @@ class FacturacionFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_facturacion, container, false)
+        var rootView: View = inflater.inflate(R.layout.fragment_facturacion,container,false)
+
+        rootView.btncerrar.setOnClickListener{
+            dismiss()
+        }
+
+        rootView.btncancelarfacturacion.setOnClickListener{
+            dismiss()
+        }
+
+        rootView.btnagregarfacturacion.setOnClickListener{
+
+            Toast.makeText(context,"Agregar Factura", Toast.LENGTH_LONG).show()
+            dismiss()
+        }
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,6 +46,7 @@ class FacturacionFragment : DialogFragment() {
         btntipopago.setOnClickListener{
             val showtipopago = TipoPagoFragment()
             showtipopago.show((activity as AppCompatActivity).supportFragmentManager,"Tipo Pago")
+            showtipopago.setCancelable(false)
         }
 
     }
