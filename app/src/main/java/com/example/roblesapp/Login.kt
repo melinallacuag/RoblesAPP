99afcaa69a10b1f7b4e66d8d03f79c8b20c2509e
Melina <melina.llacua@gmail.com>
2022-12-14 16:08:35 -0500

    MODAL DE FACTURACION DE ADAPTACION

---
+++ app/src/main/java/com/example/roblesapp/Login.kt
@@ -0,0 +1,13 @@
+package com.example.roblesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
