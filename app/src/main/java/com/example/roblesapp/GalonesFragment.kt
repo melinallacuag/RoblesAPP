50237ea2d51a05e04a36272b27db7562e6e4692f
Melina <melina.llacua@gmail.com>
2022-12-14 12:05:28 -0500

    se activo los datos del modal galones cada boton

--- app/src/main/java/com/example/roblesapp/GalonesFragment.kt
+++ app/src/main/java/com/example/roblesapp/GalonesFragment.kt
@@ -5,56 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import kotlinx.android.synthetic.main.fragment_galones.*
+import kotlinx.android.synthetic.main.fragment_galones.view.*
+import kotlinx.android.synthetic.main.fragment_soles.*
+import kotlinx.android.synthetic.main.fragment_soles.view.*
+import kotlinx.android.synthetic.main.fragment_soles.view.btncerrar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GalonesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GalonesFragment : DialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_galones, container, false)
+        var rootView: View = inflater.inflate(R.layout.fragment_galones,container,false)
+
+        rootView.btncerrar.setOnClickListener{
+            dismiss()
+        }
+
+        rootView.btncancelargalones.setOnClickListener{
+            dismiss()
+        }
+
+        rootView.btnagregargalones.setOnClickListener{
+
+            val galonesId = inputmontogalones.id
+            val galones = rootView.findViewById<EditText>(galonesId)
+
+            var totalgalones = galones.text.toString()
+
+            Toast.makeText(context,"Monto Galones: $totalgalones", Toast.LENGTH_LONG).show()
+            dismiss()
+        }
+        return rootView
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GalonesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GalonesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
