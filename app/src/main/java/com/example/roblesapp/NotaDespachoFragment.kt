a523d03d354391bb03081094fa14dd6c481c85cb
Melina <melina.llacua@gmail.com>
2022-12-14 12:28:17 -0500

    se activo los datos del modal nota de despacho cada boton

--- app/src/main/java/com/example/roblesapp/NotaDespachoFragment.kt
+++ app/src/main/java/com/example/roblesapp/NotaDespachoFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.fragment_factura.*
+import kotlinx.android.synthetic.main.fragment_facturacion.view.*
 import kotlinx.android.synthetic.main.fragment_nota_despacho.*
+import kotlinx.android.synthetic.main.fragment_nota_despacho.view.*
+import kotlinx.android.synthetic.main.fragment_soles.view.*
+import kotlinx.android.synthetic.main.fragment_soles.view.btncerrar
 
 class NotaDespachoFragment : DialogFragment() {
 
@@ -18,7 +23,22 @@ class NotaDespachoFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nota_despacho, container, false)
+        var rootView: View = inflater.inflate(R.layout.fragment_nota_despacho,container,false)
+
+        rootView.btncerrar.setOnClickListener{
+            dismiss()
+        }
+
+        rootView.btncancelarndespacho.setOnClickListener{
+            dismiss()
+        }
+
+        rootView.btnagregarndespacho.setOnClickListener{
+
+            Toast.makeText(context,"Agregar Nota Despacho", Toast.LENGTH_LONG).show()
+            dismiss()
+        }
+        return rootView
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
